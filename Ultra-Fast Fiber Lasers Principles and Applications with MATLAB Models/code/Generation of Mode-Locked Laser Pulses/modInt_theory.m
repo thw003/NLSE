5ad3bab1ceaf78b673805ed8e01509c88f0ef758
luosaiyu <905645238@qq.com>
2017-03-17 12:16:57 +0800

    check

--- Ultra-Fast Fiber Lasers Principles and Applications with MATLAB Models/code/Generation of Mode-Locked Laser Pulses/modInt_theory.m
+++ Ultra-Fast Fiber Lasers Principles and Applications with MATLAB Models/code/Generation of Mode-Locked Laser Pulses/modInt_theory.m
@@ -11,11 +11,13 @@ function Eout = modInt_theory(Ein,m,fm)
 %	modulation frequency: fm
 global Ts;
 N = size(Ein,1);
-k = (1:N)';
+k = (1:N)'; % no something like fftshift, which means that it is in the time domain.
 % k-N/2
 % -511
 % -510
 % ...
 % 512
 Eout = Ein.*exp(-m/4*(2*pi*fm*Ts)^2*(k-N/2).*(k-N/2));  % Eq. (2.12)
+                                                        % k-N/2 is to move the shape to the center, otherwise only half the pulse will appear.
+                                                        % (k-N/2)*Ts is t
 end
